749344fb7e8f0492b72118d9c434d8b99c5cc172
Garrett Limon <glimon@umich.edu>
2019-04-11 07:31:33 -0600

    pushing previous changes

--- scripts/ml_funcs.py
+++ scripts/ml_funcs.py
@@ -4,7 +4,8 @@ from sklearn.preprocessing import MinMaxScaler
 
 def extract_data_array(dataset,
                        variables,
-                       lev):
+                       lev,
+                       nf):
 
     # allocate the array
     arr = np.empty(shape=[dataset.time.size, 
@@ -25,58 +26,53 @@ def extract_data_array(dataset,
             else:
                 raise ValueError("Unsupported variable dimensions: {dims}".format(dims=dimensions))
         else:
-            values = (dataset['hyam'].values[lev] * dataset['P0'].values + 
-                      dataset['hybm'].values[lev] * dataset['PS'].values[:,:,:]) 
+            if nf==1:
+                values = (dataset['hyam'].values[lev] * dataset['P0'].values + 
+                          dataset['hybm'].values[lev] * dataset['PS'].values[:,:,:]) 
+            else:
+                values = (dataset['hyam'].values[0,lev] * dataset['P0'].values[0] + 
+                          dataset['hybm'].values[0,lev] * dataset['PS'].values[:,:,:]) 
+                
 
             # add the values into the array at the variable's position
         arr[:, :, :, var_index] = values
     
     return arr
     
-    
-def extract_features_labels(netdcf_features, 
-                            netcdf_labels,
-                            feature_vars,
-                            label_vars,
-                            level=0):
-    """
-    Extracts feature and label data from specified NetCDF files for a single level as numpy arrays.
-    
-    The feature and label NetCDFs are expected to have matching time, level, lat, and lon coordinate variables.
-    
-    Returns two arrays: the first for features and the second for labels. Arrays will have shape (time, lat, lon, var),
-    where var is the number of feature or label variables. For example if the dimensions of feature data variables in 
-    the NetCDF is (time: 360, lev: 26, lat: 120, lon: 180) and the features specified are ["T", "U"] then the resulting
-    features array will have shape (360, 120, 180, 2), with the first feature variable "T" corresponding to array[:, :, :, 0]
-    and the second feature variable "U" corresponding to array[:, :, :, 1].
-    
-    :param netdcf_features: one or more NetCDF files containing feature variables, can be single file or list
-    :param netdcf_features: one or more NetCDF files containing label variables, can be single file or list
-    :param feature_vars: list of feature variable names to be extracted from the features NetCDF
-    :param label_vars: list of label variable names to be extracted from the labels NetCDF
-    :param level: index of the level to be extracted (all times/lats/lons at this level for each feature/label variable)
-    :return: two 4-D numpy arrays, the first for features and the second for labels
-    """
-    
-    # open the features (flows) and labels (tendencies) as xarray DataSets
-    ds_features = xr.open_mfdataset(paths=netdcf_features)
-    ds_labels = xr.open_mfdataset(paths=netcdf_labels)
+def extract_datasets(data_dir,training_features,testing_features):    
+    netcdf_features_train = []
+    netcdf_labels_train = []
+    netcdf_features_predict = []
+    netcdf_labels_predict = []
 
+    for nf in range(len(training_features)):    
+        flnm = data_dir + training_features[nf]
+        netcdf_features_train.append(flnm)
+        netcdf_labels_train.append(flnm.replace('h0','h1'))
+
+    for nf in range(len(testing_features)):    
+        flnm = data_dir + testing_features[nf]
+        netcdf_features_predict.append(flnm)
+        netcdf_labels_predict.append(flnm.replace('h0','h1'))
+        
+    features_train = xr.open_mfdataset(netcdf_features_train)
+    labels_train = xr.open_mfdataset(netcdf_labels_train)
+    features_predict = xr.open_mfdataset(netcdf_features_predict)
+    labels_predict = xr.open_mfdataset(netcdf_labels_predict)
+
+    check_datasets(features_train,labels_train)
+    return features_train,labels_train,features_predict,labels_predict
+
+def check_datasets(ds1,ds2):
     # confirm that we have datasets that match on the time, lev, lat, and lon dimension/coordinate
-    if np.any(ds_features.variables['time'].values != ds_labels.variables['time'].values):
+    if np.any(ds1.variables['time'].values != ds2.variables['time'].values):
         raise ValueError('Non-matching time values between feature and label datasets')
-    if np.any(ds_features.variables['lev'].values != ds_labels.variables['lev'].values):
+    if np.any(ds1.variables['lev'].values != ds2.variables['lev'].values):
         raise ValueError('Non-matching level values between feature and label datasets')
-    if np.any(ds_features.variables['lat'].values != ds_labels.variables['lat'].values):
+    if np.any(ds1.variables['lat'].values != ds2.variables['lat'].values):
         raise ValueError('Non-matching lat values between feature and label datasets')
-    if np.any(ds_features.variables['lon'].values != ds_labels.variables['lon'].values):
+    if np.any(ds1.variables['lon'].values != ds2.variables['lon'].values):
         raise ValueError('Non-matching lon values between feature and label datasets')
-
-    # extract feature and label arrays at the specified level
-    array_features = extract_data_array(ds_features, feature_vars, level)
-    array_labels = extract_data_array(ds_labels, label_vars, level)
-    
-    return array_features, array_labels
 
 # function to perform scaling
 def scale_4d(features_train,
--- scripts/run_cnn.py
+++ scripts/run_cnn.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-
 import argparse
 import numpy as np
 import xarray as xr
 from keras.models import Sequential
 from keras.layers import Dense, Conv3D
-from ml_funcs import extract_data_array, extract_features_labels, scale_4d
+from ml_funcs import extract_data_array, extract_datasets, scale_4d
 from sklearn.preprocessing import MinMaxScaler
 
 parser = argparse.ArgumentParser(description= """ \n
@@ -42,12 +41,12 @@ Example usage of this script would be:
 
 parser.add_argument('data_dir', type=str, 
                     help='Path to the directory containing data.')
-parser.add_argument('result_dir', type=str, 
-                    help='Path to the directory to write our predicted data.')
-parser.add_argument('-training_features', type=str, nargs='+', 
+parser.add_argument('result_path', type=str, 
+                    help='PAth + filename for output netcdf file.')
+parser.add_argument('--training_features', type=str, nargs='+', 
                     help='List of file names of training features data inside data_dir. Corresponding to .h0. '+
                     '<name> files. Separate files with spaces.')
-parser.add_argument('-testing_features', type=str, nargs='+', 
+parser.add_argument('--testing_features', type=str, nargs='+', 
                     help='List of file names of predicted features data inside data_dir. Corresponding to .h0. ' +
                     ' <name> files. Separate files with spaces.')
 parser.add_argument('--features', type=str, nargs='+', default=["P","T","U","V"],
@@ -73,7 +72,7 @@ parser.add_argument('--data_format', type=str, default="channels_last",
                     help='See Keras documentation to see alternate choices.')
 parser.add_argument('--padding', type=str, default="same",
                     help='See Keras documentation to see alternate choices.')
-parser.add_argument('--epochs', type=int, default=100,
+parser.add_argument('--epochs', type=int, default=32,
                     help='Number of epochs (iterations) to use when "fitting" Neural Net. See Keras +' 
                     'documentation to see alternate choices.')
 parser.add_argument('--shuffle', type=str, default="true",
@@ -92,25 +91,16 @@ if (args.shuffle=='false'):
 else:
     shuff = True
 
-netcdf_features_train = {}
-netcdf_labels_train = {}
-netcdf_features_predict = {}
-netcdf_labels_predict = {}
-netcdf_labels_predicted = {}
+# Extract all datasets once. Reduces time spent opening/reading files
+features_train,labels_train,features_predict,labels_predict = extract_datasets(
+    args.data_dir,args.training_features,args.testing_features)
 
-#Append data_dir and result_dir to appropriate files.
-for nf in range(len(args.training_features)):
-    netcdf_features_train[nf] = args.data_dir + args.training_features[nf]
-    netcdf_labels_train[nf] = netcdf_features_train[nf].replace('.h0.','.h1.')
-for nf in range(len(args.testing_features)):
-    flnm = args.testing_features[nf]
-    netcdf_features_predict[nf] = args.data_dir + flnm
-    netcdf_labels_predict[nf] = args.data_dir + flnm.replace('.h0.','.h1.')
-    netcdf_labels_predicted[nf] = args.result_dir + flnm.replace('.h0.','.h1.')[0:len(flnm)-3]+'_predicted.nc'
-
-dmy = xr.open_dataset(netcdf_labels_predict[0])
-
-size_lev = dmy.lev.size #Zero right now because only one file
+size_lev = labels_predict.lev.size 
+size_times_train = labels_train.time.size
+size_times_predict = labels_predict.time.size
+size_lat = labels_predict.lat.size           
+size_lon = labels_predict.lon.size           
+prediction = np.empty(shape=(size_times_predict, size_lev, size_lat, size_lon))    
 
 # loop over all levels
 for lev in range(size_lev):
@@ -118,23 +108,26 @@ for lev in range(size_lev):
     print("Training/predicting for level {level}".format(level=lev))
     
     # get the features and labels for training
-    train_x, train_y = extract_features_labels(netcdf_features_train[0], #Zero right now because only one file
-                                               netcdf_labels_train[0], #Zero right now because only one file
-                                               args.features,
-                                               args.labels,
-                                               lev)
+    # train_x, train_y = extract_data_array(features_train, 
+    #                                            labels_train, 
+    #                                            args.features,
+    #                                            args.labels,
+    #                                            lev,
+    #                                            len(args.training_features))
+    train_x = extract_data_array(features_train,
+                                   args.features,
+                                   lev,
+                                   len(args.training_features))
+    train_y = extract_data_array(labels_train,
+                                   args.labels,
+                                   lev,
+                                   len(args.training_features))
     
     # get the features for prediction
-    predict_x = extract_data_array(xr.open_dataset(netcdf_features_predict[0]), #Zero right now because only one file
+    predict_x = extract_data_array(features_predict,
                                    args.features,
-                                   lev)
-
-    # I added this, James did not seem to have this in the loop and just kept
-    # the values from level zero
-    size_times_train = train_x.shape[0]
-    size_times_predict = predict_x.shape[0]
-    size_lat = train_x.shape[1]
-    size_lon = train_x.shape[2]
+                                   lev,
+                                   len(args.testing_features))
 
     # scale the data between 0 and 1
     scalers_x = [MinMaxScaler(feature_range=(0, 1))] * len(args.features)
@@ -172,13 +165,13 @@ for lev in range(size_lev):
     # reshape the data for cnn layer model input
     shape_x = (1, ) + scaled_train_x.shape
     shape_y = (1, ) + scaled_train_y.shape
+    shape_predict_x = (1, ) + scaled_predict_x.shape
     train_x = np.reshape(scaled_train_x, newshape=shape_x)
     train_y = np.reshape(scaled_train_y, newshape=shape_y)
-    predict_x = np.reshape(scaled_predict_x, newshape=shape_x)
+    predict_x = np.reshape(scaled_predict_x, newshape=shape_predict_x)
     
     # train the models
     model.fit(train_x, train_y, shuffle=shuff, epochs=int(args.epochs), verbose=args.verbose_fit)
-    prediction = np.empty(shape=(size_times_predict, size_lev, size_lat, size_lon))    
 
     # use the fitted models to make predictions
     predict_y_scaled = model.predict(predict_x, verbose=args.verbose_predict)
@@ -194,12 +187,11 @@ for lev in range(size_lev):
     level_shape = (size_times_predict, size_lat, size_lon)
     prediction[:, lev, :, :] = np.reshape(unscaled_predict_y, newshape=level_shape)
 
-print('max_prediction:', prediction.max())
-# Create Data Array from predicted data
-A = xr.DataArray(prediction,
-                 coords=dmy[args.labels[0]].coords,
-                 dims=dmy[args.labels[0]].dims,
-                 attrs=dmy[args.labels[0]].attrs)
+D = {} # Dictionary to store predicted and corresponding original data
+for label in args.labels:
+    D[label] = labels_predict[label]
+    D[label+'_predicted'] = xr.DataArray(prediction,coords=labels_predict[label].coords,
+                                         dims=labels_predict[label].dims,attrs=labels_predict[label].attrs)
 # Create Dataset with predicted and original data
-ds_predicted = xr.Dataset({args.labels[0]:dmy[args.labels[0]],args.labels[0]+'_p':A})
-ds_predicted.to_netcdf(netcdf_labels_predicted[0]) #Zero right now because only one file
+ds_predicted = xr.Dataset(D)
+ds_predicted.to_netcdf(args.result_path) #Zero right now because only one file
